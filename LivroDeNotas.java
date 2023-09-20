7baf43c22330aa0a51d04b6cfdc3c56fb01bba9a
gabriel assis <gassis574@gmail.com>
2023-09-19 21:36:52 -0300

    ap06(POO): aula concluida

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -2,6 +2,16 @@ public class LivroDeNotas{
     
     private String nomeDoCurso;
 
+    //construtor com parâmetro
+    public LivroDeNotas(String nomeDoCurso){
+        this.nomeDoCurso = nomeDoCurso;
+    }
+
+    //construtor padrão
+    public LivroDeNotas(){
+        
+    }
+
     //métodos de acesso - envia - getters - public
     public String getNomeDoCurso(){
         return nomeDoCurso;
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -4,16 +4,16 @@ public class TesteLivroDeNotas {
 
     public static void main(String[] args) {
 
-        LivroDeNotas livroDeNotas1 = new LivroDeNotas();
-        LivroDeNotas livroDeNotas2 = new LivroDeNotas();
-        
         String nome1 = JOptionPane.showInputDialog("digite o curso: ");
+        LivroDeNotas livroDeNotas1 = new LivroDeNotas(nome1);
         livroDeNotas1.setNomeDoCurso(nome1);
-        JOptionPane.showMessageDialog(null, livroDeNotas1.getNomeDoCurso(),"o curso:", JOptionPane.INFORMATION_MESSAGE);
 
         String nome2 = JOptionPane.showInputDialog("digite o curso novamente: ");
+        LivroDeNotas livroDeNotas2 = new LivroDeNotas(nome2);
         livroDeNotas2.setNomeDoCurso(nome2);
-        JOptionPane.showMessageDialog(null, livroDeNotas2.getNomeDoCurso(),"o curso:", JOptionPane.INFORMATION_MESSAGE);
+
+        livroDeNotas1.exibirMensagem(nome1);
+        livroDeNotas2.exibirMensagem(nome2);
 
     }
     
